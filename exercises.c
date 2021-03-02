e3017d9ba345161c9488486a2aa20189d44c18e6
repl.it user <replituser@example.com>
2021-03-02 01:37:33 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -37,7 +37,8 @@ Utilice la función sumaN.
 */
 
 void sumaNultimos(int a[], int n, int m, int * suma) {
-  *suma = sumaN(a+(m) , m);
+  printf("%lu", sizeof(*a)/sizeof(int) );
+  *suma = sumaN(a+m, m);
 }
 
 /*
